eda458e266a6a15f81bec0469268a93cffb44bb1
Max <maxbui0911@gmail.com>
2023-01-02 17:29:42 +0100

    env

--- src/store/restaurants/actions.tsx
+++ src/store/restaurants/actions.tsx
@@ -1,5 +1,6 @@
 export const getRestaurants = (): () => Promise<any> => async () => {
     let res;
+    console.log(process.env.REACT_APP_API_KEY);
     await fetch(process.env.REACT_APP_API_KEY + "restaurants", {
         method: "GET",
         headers: { "Content-Type": "application/json" }
@@ -7,6 +8,7 @@ export const getRestaurants = (): () => Promise<any> => async () => {
         if (!response.ok) {
             console.log("error");
         }
+        console.log(response);
         return res = response.json();
     }).catch(err => {
         return err;
